842648ffef621540742149d39fe55b3f4d7221ec
Alex Fortuna <fortunadze@gmail.com>
2023-10-19 11:49:10 +0300

    Moving on

--- lib/rspec_magic.rb
+++ lib/rspec_magic.rb
@@ -1,7 +1,4 @@
 
-# Load public features.
+# Load the "world" features.
 require_relative "rspec_magic/config"
 require_relative "rspec_magic/version"
-
-# Load stable features.
-require_relative "rspec_magic/stable"
