f85be6d752c103eb8e76384342a1c9b718e655e5
Alex Fortuna <fortunadze@gmail.com>
2023-12-10 23:11:07 +0300

    Moving on

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,5 @@
 # Must go before all.
 require_relative "support/simplecov"
 
-# Self.
-require_relative "../lib/rspec_magic"
-require_relative "../lib/rspec_magic/unstable"
-
-RSpecMagic::Config.spec_path = __dir__
-
 # Load support files. Sorted alphabetically for greater control.
 Dir[File.expand_path("support/**/*.rb", __dir__)].sort.each { |fn| require fn }
