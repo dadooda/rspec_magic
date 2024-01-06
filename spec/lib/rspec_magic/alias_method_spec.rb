73f428cd600872e0818d32e15f997ccca2100706
Alex Fortuna <fortunadze@gmail.com>
2024-01-06 23:59:31 +0300

    Moving on

--- spec/lib/rspec_magic/alias_method_spec.rb
+++ spec/lib/rspec_magic/alias_method_spec.rb
@@ -3,14 +3,17 @@ describe "`alias_method` matcher" do
   describe "klass" do
     let(:klass) do
       Class.new do
-        def is_one
-        end
-        alias_method :one?, :is_one
+        def one; end
+        alias_method :one1, :one
+
+        def two; end
+        alias two2 two
       end
     end
 
     subject { klass.new }
 
-    it { is_expected.to alias_method(:one?, :is_one) }
+    it { is_expected.to alias_method(:one1, :one) }
+    it { is_expected.to alias_method(:two2, :two) }
   end
 end
